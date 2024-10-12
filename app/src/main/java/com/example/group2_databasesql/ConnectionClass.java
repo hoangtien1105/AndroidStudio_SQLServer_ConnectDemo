8827c95cb26269c8f7c8fe3f6c10180c915d4456
Hoang Tien <minhtien11052003@gmail.com>
2024-10-12 15:48:07 +0700

    hi
    uy

--- app/src/main/java/com/example/group2_databasesql/ConnectionClass.java
+++ app/src/main/java/com/example/group2_databasesql/ConnectionClass.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 public class ConnectionClass {
     String classes = "net.sourceforge.jtds.jdbc.Driver";
-    protected static String ip="10.0.2.2";//192.168.56.1
+    protected static String ip="10.87.29.124";//192.168.56.1
     protected static  String port="1433";
     protected static String db="SaleManagement";
     protected static String un="sa";
@@ -20,13 +20,13 @@ public class ConnectionClass {
         StrictMode.setThreadPolicy(policy); // thiet lap chinh sac ket noi bao gom tat ca policy
         Connection conn = null;
         try {
-            Class.forName(classes);
+          //  Class.forName(classes);
            // String conUrl= "jdbc:jtds:sqlserver://"+ ip + ":"+ port+";"+ "databasename="+ db+";user="+un+";password="+password+";";
             String conUrl= "jdbc:jtds:sqlserver://"+ ip + ":"+ port+";"+ db;
             conn = DriverManager.getConnection(conUrl,un,password);
 
 
-        }catch (ClassNotFoundException| SQLException e){
+        }catch ( SQLException e){
             throw  new RuntimeException(e);
         }
         return conn;
